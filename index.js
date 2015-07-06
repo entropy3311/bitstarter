ddc378caa9f6ade3d8518e0dd1965546a8c13e88
entropy3311 <entropy3311@gmail.com>
2015-07-06 04:53:21 +0000

    read html

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var fs = require('fs');
+var express = require('express')
+var fs = require('fs')
 
 
 
 
-var app = express.createServer(express.logger());
+var app = express.createServer(express.logger())
 
 
 app.get('/', function(request, response) {
-  response.send((fs.readFileSync('index.html');
+  response.send((fs.readFileSync('index.html')
  //  response.send('index.html')
 })
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000
 app.listen(app.get('port'), function() {
   console.log("Listening on" + app.get('port'))
 })
